513da07db93ef3e1d40921e951bbc0f60dc8b0b1
Merlina <noahchukita@gmail.com>
2024-05-12 14:55:59 -0300

    Creando vista Contacto

---
+++ front/src/app/contact/page.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import styles from '../../components/CardHome/CardHome.module.css'
+
+const Contact = () => {
+  return (
+    <div className='my-16'>
+    <h1 className='text-center text-3xl font-bold mb-8'>Contact Us</h1>
+    <form className='w-2/5 ml-auto mr-auto p-5 bg-[#f5f5f5] rounded shadow-md'>
+      <label className='block my-2.5 font-bold'>Name</label>
+      <input className='w-full p-2.5 border border-slate-300 rounded text-base mb-2.5' type="text" />
+
+      <label className='block my-2.5 font-bold'>Email</label>
+      <input className='w-full p-2.5 border border-slate-300 rounded text-base mb-2.5' type="text" />
+
+      <label className='block my-2.5 font-bold'>Phone</label>
+      <input className='w-full p-2.5 border border-slate-300 rounded text-base mb-2.5' type="text" />
+
+      <label className='block my-2.5 font-bold'>Message</label>
+      <textarea className='w-full p-2.5 border border-slate-300 rounded text-base mb-2.5' />
+      
+
+      <button className={`${styles.button} flex justify-center items-center gap-4 shadow-md w-full border-slate-800 bg-[#1A1A1A] text-white text-sm font-medium mt-8 hover:-translate-y-1 transition p-3 duration-300 rounded border-2`}>Enviar</button>
+
+    </form>
+  </div>
+  )
+}
+
+export default Contact

f05fdc8f2f242ad24bd8b06014afcfb7390fd48d
Merlina <noahchukita@gmail.com>
2024-05-12 14:55:09 -0300

    Agregando mensaje vista Compras cuando no hay compras realizadas

--- front/src/app/purchases/page.tsx
+++ front/src/app/purchases/page.tsx
@@ -2,6 +2,8 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import ProductCard from '@/components/ProductCard/ProductCard';
+import styles from '../../components/CardHome/CardHome.module.css'
+import Link from 'next/link';
 
 const Purchases = () => {
   const storedUserSession = localStorage.getItem("userSession");
@@ -27,10 +29,10 @@ const Purchases = () => {
   return (
     <div>
         <h2 className='text-2xl font-bold text-[#454545] mb-4 mt-10'>Mis compras</h2>
-        <div className='flex gap-8 my-12'>
+        <div>
             {
-                userPurchaseData ? (
-                    <>
+                userPurchaseData.length > 0 ? (
+                    <div className='flex gap-8 my-12'>
                         {
                             userPurchaseData.map(purchase => (
                                 <>
@@ -42,10 +44,14 @@ const Purchases = () => {
                                 </>
                             ))
                         }
-                    </>
+                    </div>
                 ) : (
-                    <>
-                    </>
+                  <div className='flex gap-8 my-12 justify-center'>
+                    <div className="flex flex-col items-center">
+                      <p className="text-lg">Aún no realizaste ninguna compra 💔</p>
+                      <button className={`${styles.button} flex justify-center items-center gap-4 shadow-sm w-72 border-slate-800 bg-[#1A1A1A] text-white text-sm font-medium mt-4 hover:-translate-y-1 transition p-3 duration-300 rounded border-2`}><Link href="/">Ver productos</Link></button>
+                    </div>
+                  </div>
                 )
             }
         </div>
